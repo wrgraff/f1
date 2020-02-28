4d3a2cf6f0994e7ef4a9d06130378f3e26c767d4
wrgraff <wrgraff@gmail.com>
2020-02-28 03:47:38 +0300

    get points and places

--- index.js
+++ index.js
@@ -58,21 +58,47 @@ function getRacesHandler(raceId, currentYear, result, race) {
 };
 
 function getUserLink(playerIndex, raceId, forecasts, successHandler) {
-	result = {};
 	needle.get('https://www.f1news.ru/forecast/race/' + raceId + '/rating/?q=' + PLAYERS[playerIndex].login, function(error, response) {
 		if (!error && response.statusCode == 200) {
 			let dom = new jsdom(response.body);
 			let link = dom.window.document.querySelector('.f1Table tbody tr td:nth-child(2) a');
 
-			result.link = link ? link.href : 'Null forecast';
+			if (link) {
+				getResult(link.href, getResultHandler);
+			} else {
+				getResultHandler('Null forecast');
+			};
 		};
 
-		forecasts[PLAYERS[playerIndex].name] = result;
+		function getResultHandler(result) {
+			forecasts[PLAYERS[playerIndex].name] = result;
 
-		if (playerIndex == PLAYERS.length - 1) {
-			successHandler(forecasts);
+			if (playerIndex == PLAYERS.length - 1) {
+				successHandler(forecasts);
+			} else {
+				getUserLink(++playerIndex, raceId, forecasts, successHandler);
+			};
+		};
+	});
+};
+
+function getResult(link, successHandler) {
+	let result = {
+		link: link,
+	};
+	needle.get('https://www.f1news.ru' + link, function(error, response) {
+		if (!error && response.statusCode == 200) {
+			let dom = new jsdom(response.body);
+			let total = dom.window.document.querySelector('#content p');
+			let points = total.textContent.replace(/\n/g, '').replace(/([aA-zZ])\w+ набрал /g, '').replace(/ .* и занял ([0-9])\w+ место./g, '');
+			let place = total.textContent.replace(/\n/g, '').replace(/([aA-zZ])\w+ набрал ([0-9])\w+ .* и занял /g, '').replace(/место./g, '');
+			
+			result.points = points ? parseInt(points) : 'Points not found';
+			result.place = place ? parseInt(place) : 'Place not found';
+			successHandler(result);
 		} else {
-			getUserLink(++playerIndex, raceId, forecasts, successHandler);
+			result.error = 'Page not loaded';
+			successHandler(result);
 		};
 	});
 };
